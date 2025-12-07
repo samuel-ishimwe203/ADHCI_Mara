c74c83520993cfbe6a306d8a5c3c4371b0e79490
samuel-ishimwe203 <samuelishimw02@gmail.com>
2025-12-07 21:49:22 +0200

    fix: database fixing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import ChatApp from './ChatApp.jsx'
-import './styles.css'  // Fixed: ./ for same-folder import
+import './styles.css  // Fixed: ./ for same-folder import
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
